2083b49513c8092536c81c318a94bac514312a03
Gene Mueller <geneemueller@gmail.com>
2023-11-05 15:46:40 -0800

    Initial Commit

---
+++ HealthInspectionapp/HealthInspectionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthInspectionappApp.swift
+//  HealthInspectionapp
+//
+//  Created by Gene Mueller on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthInspectionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
